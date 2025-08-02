c2708ce1663aa875a99330f15188461658200c6a
Odiljon Bobosodikov <bobosodikov0d@gmail.com>
2025-08-02 05:53:08 +0500

    Initial commit

---
+++ lib/pages/intro.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:sneakers_shop/utils/colors.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(25),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(25),
+                child: Image.asset(
+                  "lib/images/logo-phone.png",
+                  color: Colors.white70,
+                ),
+              ),
+
+              Text(
+                "EMBRACE SIMPLICITY",
+                style: TextStyle(color: AppColors.secondary, fontSize: 16),
+
+                textAlign: TextAlign.center,
+              ),
+
+              const SizedBox(height: 8),
+
+              Text(
+                "— Terry A. Davis",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: AppColors.secondary,
+                ),
+
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 100),
+              GestureDetector(
+                //onTap: () => Navigator.push(
+                //  context,
+                //  MaterialPageRoute(builder: (context) => ()),
+                //),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: AppColors.primary,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  padding: EdgeInsets.all(25),
+                  child: Center(
+                    child: Text(
+                      "Shop Now",
+                      style: TextStyle(
+                        color: AppColors.background,
+                        fontWeight: FontWeight.w900,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/utils/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary =  Color(0xFFD71921);
+  static const Color secondary = Color(0xFF3F3F42);
+  static const Color background = Color(0xFF1B1B1D);
+}
